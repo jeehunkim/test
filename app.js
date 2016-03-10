5a0d591a0f27e47378c874f64f68ad412457f7d6
jeehunkim <nob98@naver.com>
2016-03-10 17:41:35 +0900

    second

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
+/*
 app.get('/', function(req,res){
   res.send('hell');
 });
+*/
+
+//app.use(express.static(__dirname + '/public'));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(8888, function(){
   console.log('Server On!');
